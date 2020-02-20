db5931fee4c99b3fe87fba3770d2290881c68788
Reto Steffen <reto.steffen@gmail.com>
2020-02-20 18:51:47 +0100

    og tags

--- resources/views/vendor/laravel-blog/blog/index.blade.php
+++ resources/views/vendor/laravel-blog/blog/index.blade.php
@@ -10,6 +10,8 @@
   <link rel="canonical" href="{{URL::current()}}">
   <meta property="og:locale" content="{{config('laravel-blog.locale')}}">
   <meta property="og:title" content="{{ config('laravel-blog.blog_name') }}">
+  <meta property="og:type" content="website">
+<meta property="og:description" content="{{ config('laravel-blog.blog_description') }}">
   <meta property="og:url" content="{{URL::current()}}">
   <meta property="og:site_name" content="{{config('laravel-blog.blog_name')}}">
   <meta property="article:publisher" content="{{config('laravel-blog.facebook_name')}}">
